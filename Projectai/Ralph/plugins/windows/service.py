42694620afadecd7c9b5fbe9e7f0d0c5f705e7ca
dragonlegendari11-beep <dragonlegendari11@gmail.com>
2026-07-05 22:43:44 +0300

    Update service.py

--- Projectai/Ralph/plugins/windows/service.py
+++ Projectai/Ralph/plugins/windows/service.py
@@ -1,7 +1,19 @@
 import subprocess
+import webbrowser
 
 
 class WindowsService:
 
     def open_program(self, program: str):
+
+        if program == "browser":
+            webbrowser.open("https://www.google.com")
+            return
+
         subprocess.Popen(program)
+
+    def shutdown(self):
+        subprocess.run(["shutdown", "/s", "/t", "0"])
+
+    def restart(self):
+        subprocess.run(["shutdown", "/r", "/t", "0"])
